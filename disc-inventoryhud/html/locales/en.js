1c0f4530bc50329480b78e47124e1c9e50d21e11
TaerAttO <50113216+MonsterTaerAttO@users.noreply.github.com>
2019-12-16 20:52:20 +0700

    +

--- disc-inventoryhud/html/locales/en.js
+++ disc-inventoryhud/html/locales/en.js
@@ -5,5 +5,3 @@ invLocale.giveItem = "Give";
 invLocale.label = "Inventory Label";
 invLocale.stealmoney = "Steal Cash";
 invLocale.seizemoney = "Seize Cash";
-invLocale.takemoney = "Take";
-invLocale.storemoney = "Store";
